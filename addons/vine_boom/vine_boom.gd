7d5415d27f652de140976c422dbb4c5bc65fcd46
cool-cat-dev <earnwizardgames@gmail.com>
2025-08-23 22:58:49 -0500

    logic changes - working build

--- addons/vine_boom/vine_boom.gd
+++ addons/vine_boom/vine_boom.gd
@@ -1,17 +1,17 @@
 @tool
 extends EditorPlugin
 
-const DOCK_NAME = "Vine Boom"
 var dock
 
 func _enter_tree():
 	# Load and instantiate the dock scene
 	dock = preload("res://addons/vine_boom/vine_boom_dock.tscn").instantiate()
-	# Add it to the editor (you can change DOCK_SLOT_LEFT_UR to other positions)
-	add_control_to_dock(DOCK_SLOT_LEFT_UR, dock)
+	
+	# Add it to the bottom panel (with Output, Debugger, etc.)
+	add_control_to_bottom_panel(dock, "Vine Boom")
 
 func _exit_tree():
 	# Clean up when disabling the plugin
-	remove_control_from_docks(dock)
+	remove_control_from_bottom_panel(dock)
 	if dock:
 		dock.queue_free()
